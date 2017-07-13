6e3087b883094cbd58284ea1e39e8e116352adcd
ablinkinz <ablinkin@me.com>
2017-07-12 23:00:42 -0400

    Added upload and latency tests

--- networktest.py
+++ networktest.py
@@ -1,4 +1,14 @@
 import pyspeedtest
+import importlib
+import pip
+
+try:
+        importlib.import_module('pyspeedtest')
+        import pyspeedtest
+except ImportError:
+        import pip
+        pip.main(['install', 'pyspeedtest'])
+        import pyspeedtest
 
 def download():
     st = pyspeedtest.SpeedTest()
@@ -10,4 +20,9 @@ def upload():
     st = pyspeedtest.SpeedTest()
     ul = st.upload()
     ul = ((ul / 1024)/1024)
-    return {"download speed":ul}
+    return {"upload speed":ul}
+
+def latency():
+    st = pyspeedtest.SpeedTest()
+    ping = st.ping()
+    return {"latency":ping}
