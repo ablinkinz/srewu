81404444e76b118a5e4b7e1a1121255e32826a70
root <root@tjones-master.c.optimum-tensor-161912.internal>
2017-07-27 22:18:08 +0000

    v0.01

--- docker/swarm.py
+++ docker/swarm.py
@@ -1,5 +1,6 @@
 import docker
-
+import os
+import subprocess
 client = docker.from_env()
 
 '''
@@ -11,10 +12,10 @@ NOTE *** there are 5
 
 
 def swarm_init(advertise_addr=str,listen_addr=int, force_new_cluster=bool ):
-    client.swarm.init(advertise_addr, listen_addr,force_new_cluster).logs()
+    client.swarm.init(advertise_addr, listen_addr,force_new_cluster)
     print('Docker swrarm has been Initalized on the minion')
+    print('Worker Join token below!')
+    os.system("docker swarm join-token worker | xargs | awk '{print $16}' ")
 
+#swarm_init(advertise_addr='ens4',listen_addr='0.0.0.0:5000', force_new_cluster=False)
 
-swarm_init(advertise_addr='ens4',listen_addr='0.0.0.0:5000', force_new_cluster=False)
-
-#def swarm_join(remote_addrs=list, join_token=str, advertise_addr=str):
