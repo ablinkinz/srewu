ebc7edd4bb309c7d1e25fd8fc4c21437de8de986
Tyler Jones <Tyler.Jones@sterlingts.com>
2017-07-27 16:26:44 -0400

    adding logging

--- docker/swarm.py
+++ docker/swarm.py
@@ -5,18 +5,16 @@ client = docker.from_env()
 '''
 Initalize Docker on Minion as a Swarm Manager
 NOTE *** there are 5
+
+
 '''
 
 
 def swarm_init(advertise_addr=str,listen_addr=int, force_new_cluster=bool ):
-    client.swarm.init(advertise_addr, listen_addr,force_new_cluster)
+    client.swarm.init(advertise_addr, listen_addr,force_new_cluster).logs()
     print('Docker swrarm has been Initalized on the minion')
-    
-
 
 
 swarm_init(advertise_addr='ens4',listen_addr='0.0.0.0:5000', force_new_cluster=False)
 
-
-
-
+#def swarm_join(remote_addrs=list, join_token=str, advertise_addr=str):
