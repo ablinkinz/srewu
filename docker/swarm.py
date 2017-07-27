a721300f7fb80a3c430ce6a8349fd4b4956e3af0
root <root@tjones-master.c.optimum-tensor-161912.internal>
2017-07-27 20:15:31 +0000

    Start a swarm

--- docker/swarm.py
+++ docker/swarm.py
@@ -1,5 +1,22 @@
 import docker
 
 client = docker.from_env()
-container = client.containers.run("ubuntu:latest", "/bin/bash", detach=True)
-container.logs()
+
+'''
+Initalize Docker on Minion as a Swarm Manager
+NOTE *** there are 5
+'''
+
+
+def swarm_init(advertise_addr=str,listen_addr=int, force_new_cluster=bool ):
+    client.swarm.init(advertise_addr, listen_addr,force_new_cluster)
+    print('Docker swrarm has been Initalized on the minion')
+    
+
+
+
+swarm_init(advertise_addr='ens4',listen_addr='0.0.0.0:5000', force_new_cluster=False)
+
+
+
+
