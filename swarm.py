3176835cb60a2b80c573ec5f9906754d6373620e
root <root@tjones-master.c.optimum-tensor-161912.internal>
2017-07-28 19:37:59 +0000

    Working

--- swarm.py
+++ swarm.py
@@ -6,23 +6,24 @@ client = docker.from_env()
 '''
 Initalize Docker on Minion as a Swarm Manager
 NOTE *** there are 5
-
+somthing
 
 '''
 
 
-def swarm_init(advertise_addr=str,listen_addr=int, force_new_cluster=bool ):
+def swarmi_init(advertise_addr=str,listen_addr=int, force_new_cluster=bool ):
     d = []
     client.swarm.init(advertise_addr, listen_addr,force_new_cluster)
     output =  'Docker swrarm has been Initalized on the minion and the worker Join token is below'
     command = "docker swarm join-token worker | xargs | awk '{print $16}' "
-    run = subprocess.check_output([command])
-    d.append({'Comment': output, 'Worker_Token': run})
+    var = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+    somthing = var.communicate()[0]
+    d.append({'Comment': output, 'Worker_Token': somthing})
     return d 
      
 
 
 
 
-#swarm_init(advertise_addr='ens4',listen_addr='0.0.0.0:5000', force_new_cluster=False)
+#swarminit(advertise_addr='ens4',listen_addr='0.0.0.0:5000', force_new_cluster=False)
 
