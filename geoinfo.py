e9ae312d2457ca11177ce9df99ef1db37dfa304f
Stephan Looney <slooney@slooney-mbp.lan>
2017-12-15 00:20:09 -0500

    added some doctests

--- geoinfo.py
+++ geoinfo.py
@@ -1,12 +1,27 @@
+'''
+doctest.testmod(srewu.geoinfo, verbose=True, optionflags=doctest.ELLIPSIS)
+'''
 import requests
 from requests import get
+import doctest
+import docstring
 
 def getexternalip():
+    '''
+    getexternalip() will make a call to ipify.org to return the external ip
+    >>> getexternalip()
+    u'http://...
+    '''
     ip = get('https://api.ipify.org').text
     url = 'http://freegeoip.net/json/'+ip
     return url
 
 def getlocation():
+    '''
+    getlocation() will make a call to ipify.org to return the external ip location info
+    >>> getlocation()
+    {...}
+    '''
     url = getexternalip()
     locationinfo = requests.get(url)
     locationinfo = locationinfo.json()
